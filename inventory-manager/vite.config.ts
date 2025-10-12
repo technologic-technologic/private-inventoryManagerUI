715e862461e4a41af84ce3c43b26801a408693fe
Leonardo Trevizo <leonardo.trevizo@encora.com>
2025-10-11 21:22:15 -0600

    Updated port to 8080
    Updated the default port in which te client runs to the specified in the requirements port 8080.
    
    ## Key changes
    - Updated server to 8080 in vite.config.ts file.
    - Upgraded proxy specifications to cleaner requests and behavior.
    
    No breaking changes, yet.

--- inventory-manager/vite.config.ts
+++ inventory-manager/vite.config.ts
@@ -1,7 +1,19 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9090',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
